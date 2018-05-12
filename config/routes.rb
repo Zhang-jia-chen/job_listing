4491b183d7139552e6551e074ac287416359f93c
Zhang-jia-chen <zjc834723074@gmail.com>
2018-05-12 16:00:21 +0800

    database.yml 加入ignore

--- config/routes.rb
+++ config/routes.rb
@@ -14,12 +14,12 @@ Rails.application.routes.draw do
     end
   end
 
-  
+
 
   resources :jobs do
     resources :resumes
   end
 
-  root 'welcome#index'
+  root 'jobs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
