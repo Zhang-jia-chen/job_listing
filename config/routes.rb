03718fa26444e97602f36b557c04a973b8d6d69d
Zhang-jia-chen <zjc834723074@gmail.com>
2018-05-12 15:49:49 +0800

    管理员可以下载应聘者上传的简历

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,17 @@ Rails.application.routes.draw do
         post :publish
         post :hide
       end
+
+      resources :resumes
     end
   end
+
+  
 
   resources :jobs do
     resources :resumes
   end
-  
+
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
