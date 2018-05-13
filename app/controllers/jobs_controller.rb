11160d7e0e55ae0500a4d06626b4c9e8d0d86208
Zhang-jia-chen <zjc834723074@gmail.com>
2018-05-13 11:10:09 +0800

    优化， 应聘者不可以对职位进行crud

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,5 @@
 class JobsController < ApplicationController
-before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+before_action :authenticate_user!, only: [:show]
   def index
     @jobs = case params[:order]
             when 'by_lower_bound'
@@ -19,43 +19,43 @@ before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destro
     end
   end
 
-  def new
-    @job = Job.new
-  end
+  # def new
+  #   @job = Job.new
+  # end
+  #
+  # def create
+  #   @job = Job.new(job_params)
+  #
+  #   if @job.save
+  #    redirect_to jobs_path
+  #   else
+  #    render 'new'
+  #   end
+  # end
+  #
+  # def edit
+  #   @job = Job.find(params[:id])
+  # end
+  #
+  # def update
+  #   @job = Job.find(params[:id])
+  #   if @job.update(job_params)
+  #     redirect_to jobs_path
+  #   else
+  #     render 'edit'
+  #   end
+  #
+  # end
+  #
+  # def destroy
+  #   @job = Job.find(params[:id])
+  #   @job.destroy
+  #   redirect_to jobs_path
+  # end
 
-  def create
-    @job = Job.new(job_params)
-
-    if @job.save
-     redirect_to jobs_path
-    else
-     render 'new'
-    end
-  end
-
-  def edit
-    @job = Job.find(params[:id])
-  end
-
-  def update
-    @job = Job.find(params[:id])
-    if @job.update(job_params)
-      redirect_to jobs_path
-    else
-      render 'edit'
-    end
-
-  end
-
-  def destroy
-    @job = Job.find(params[:id])
-    @job.destroy
-    redirect_to jobs_path
-  end
-
-  private
-
-  def job_params
-    params.require(:job).permit(:title, :description, :wage_lower_bound, :wage_upper_bound, :contact_email, :is_hidden)
-  end
+  # private
+  #
+  # def job_params
+  #   params.require(:job).permit(:title, :description, :wage_lower_bound, :wage_upper_bound, :contact_email, :is_hidden)
+  # end
 end
